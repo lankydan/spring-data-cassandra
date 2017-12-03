423417868e1057aa75390ed687acbd33c8044a7a
Dan Newton <danknewton@hotmail.com>
2017-12-03 16:16:36 +0000

    commit

--- src/main/java/com/lankydan/cassandra/CassandraConfig.java
+++ src/main/java/com/lankydan/cassandra/CassandraConfig.java
@@ -1,7 +1,6 @@
 package com.lankydan.cassandra;
 
 import java.util.List;
-import org.springframework.beans.factory.BeanClassLoaderAware;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
